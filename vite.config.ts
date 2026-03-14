0ffbf64abb8c01e2cda236e4249dad3a0ad869a0
Alex <panitroll33@gmail.com>
2026-03-14 12:20:10 +0100

    fix github pages (again)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: process.env.GITHUB_ACTIONS ? '/your-repo-name/' : '/'
+    base: process.env.GITHUB_ACTIONS ? '/Portfolio/' : '/'
 })
